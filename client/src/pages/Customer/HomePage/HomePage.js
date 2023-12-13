c86d3759fffdb5ce4822fef8aff5a4b95af438d8
hoangs39 <s3927241@rmit.edu.vn>
2023-12-13 21:58:05 +0700

    DEAR HUNG LAN 2

--- client/src/pages/Customer/HomePage/HomePage.js
+++ client/src/pages/Customer/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import Header from "../../../components/header";
 import Footer from "../../../components/footer";
 import Banner from "../../../components/Banner";
-import Promotion from "../../../components/Promotion";
+import Promotion from "../../../components/Carousel";
 function HomePage() {
     return (
         <>
